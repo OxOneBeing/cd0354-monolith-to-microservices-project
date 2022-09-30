5699480b2872c1f52c5a737d271ff65d51046843
Sunday Oruwhone <sundayoruwhone@gmail.com>
2022-09-30 15:54:54 +0100

    initial commit

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -18,6 +18,7 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
 
   const app = express();
   const port = process.env.PORT || 8080;
+  // const port = process.env.PORT || 8100;
 
   app.use(bodyParser.json());
 
@@ -45,7 +46,7 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
 
   // Start the Server
   app.listen( port, () => {
-    console.log( `server running ${config.url}` );
+    console.log( `server running at ${config.url}` );
     console.log( `press CTRL+C to stop server` );
   } );
 })();
